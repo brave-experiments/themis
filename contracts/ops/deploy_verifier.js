696b0805a54facb698db4a7a4cf2d7871727cfa3
gpestana <g6pestana@gmail.com>
2019-08-29 09:43:14 +0100

    sol contracts

--- contracts/ops/deploy_verifier.js
+++ contracts/ops/deploy_verifier.js
@@ -1,11 +1,5 @@
 const Web3 = require("web3");
-const Payment = require("../build/contracts/Payment.json")
-const Escrow = require("../build/contracts/Escrow.json")
-
-const BurnVerifier = require("../zether-contracts/BurnVerifier.json");
-const CashToken = require("../zether-contracts/CashToken.json");
-const ZetherVerifier = require("../zether-contracts/ZetherVerifier.json");
-const ZSC = require("../zether-contracts/ZSC.json");
+const Verifer = require("../build/contracts/Escrow.json")
 
 async function deploy(web3, from, gas, contractObj, args) {
     const abi = contractObj.abi;
@@ -24,27 +18,15 @@ async function deployContracts(web3) {
     const n1addr = "0xed9d02e382b34818e88b88a309c7fe71e65f419d"
     const gas = 470000000;
     const policyArr = [1, 3, 4, 1, 3]; // TODO: encrypt
-    let escrowAccountAddr;
+    let receiptVerifierContract;
 
     // Escrow contract
     try {
-        const receiptEscrowContract = await deploy(web3, n1addr, gas, Escrow, [])
-        console.log("\n EscrowContract Address: ", receiptEscrowContract.contractAddress)
+        const receipt = await deploy(web3, n1addr, gas, Verifer, [])
+        console.log("\n VeriferContract Address: ", receipt.contractAddress)
 
-        // updates escrowAccountAddr
-        escrowAccountAddr = receiptEscrowContract.contractAddress;
-    } catch(err) {
-        console.log(err)
-    }
-
-    // Payment contract
-    try {
-        const args = [];
-        args.push(policyArr);
-        args.push(escrowAccountAddr);
-
-        const receiptPaymentContract = await deploy(web3, n1addr, gas, Payment, args)
-        console.log("\n PaymentContract Address: ", receiptPaymentContract.contractAddress)
+        // updates receiptVerifierContract
+        receiptVerifierContract = receipt.contractAddress;
     } catch(err) {
         console.log(err)
     }
