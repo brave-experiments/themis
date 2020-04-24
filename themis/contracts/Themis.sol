c18409fd38166f1f34cf4667898cf6c2843feb1b
iquerejeta <querejeta.inigo@gmail.com>
2020-04-24 13:26:07 +0100

    ciphertext as 2 points

--- themis/contracts/Themis.sol
+++ themis/contracts/Themis.sol
@@ -11,43 +11,54 @@ contract ThemisPolicyContract {
   }
 
   struct PaymentRequests {
-    uint256[2] encrypted_aggregate;
+    uint256[4] encrypted_aggregate;
     uint256[2] decrypted_aggregate;
     uint256[2] proof_correct_decryption;
     bool valid;
   }
 
   // public storage
-  mapping (bytes32 => uint256[2][]) aggregate_storage;
+  mapping (bytes32 => uint256[4][]) aggregate_storage;
   mapping (bytes32 => PaymentRequests[]) payment_requests;
 
-  event Aggregate(uint256[2] aggr);
+  event Aggregate(uint256[4] aggr);
   event DoneProof();
 
   // TODO: define constructor
   constructor() public {}
 
+  // public 
+  function add_points_and_check(
+    uint256[2][3] memory input
+  ) payable public returns (uint256[2] memory) {
+    uint256[2] memory addition = _bn128_add([input[0][0], input[0][1], input[1][0], input[1][1]]);
+
+    require(addition[0] == input[2][0] && addition[1] == input[2][1], "equality failed");
+    
+    return [input[0][0], input[0][1]];
+
+  }
   // public
   function calculate_aggregate(
-    uint256[2][length_policies] memory input, 
+    uint256[4][length_policies] memory input, 
     bytes32 client_id
-  ) payable public returns (uint256[2] memory) {
+  ) payable public returns (uint256[4] memory) {
 
     // calculates aggregate
-    uint256[2] memory aggregate = _inner_product(
+    uint256[4] memory aggregate = _inner_product(
       input,
       policies
     );
 
     // stores aggregate in array keyed by client_id && return aggregate to caller
-    uint256[2][] storage aggregates_client = aggregate_storage[client_id];
+    uint256[4][] storage aggregates_client = aggregate_storage[client_id];
     aggregates_client.push(aggregate);
     
     return aggregate;
   }
 
   function request_payment(
-    uint256[2] memory encrypted_aggregate,
+    uint256[4] memory encrypted_aggregate,
     uint256[2] memory decrypted_aggregate,
     uint256[2] memory proof_correct_decryption,
     bytes32 client_id
@@ -68,20 +79,30 @@ contract ThemisPolicyContract {
   }
 
   // private
-  function _inner_product(uint256[2][length_policies] memory ciphertext_vector, uint256[length_policies] memory scalar_vector) private returns (uint256[2] memory) {
-      uint256[2] memory aggregate;
-      uint256[2] memory resultMult;
+  function _inner_product(uint256[4][length_policies] memory ciphertext_vector, uint256[length_policies] memory scalar_vector) private returns (uint256[4] memory) {
+      uint256[2] memory aggregate_1;
+      uint256[2] memory aggregate_2;
+      uint256[2] memory resultMult_1;
+      uint256[2] memory resultMult_2;
 
       for (uint i = 0; i < length_policies; i++) {
-        resultMult = _bn128_multiply([
+        resultMult_1 = _bn128_multiply([
           ciphertext_vector[i][0],
           ciphertext_vector[i][1], 
           scalar_vector[i]
         ]);  
 
-        aggregate = _bn128_add([resultMult[0], resultMult[1], aggregate[0], aggregate[1]]);
+        resultMult_2 = _bn128_multiply([
+          ciphertext_vector[i][2], 
+          ciphertext_vector[i][3],
+          scalar_vector[i]
+        ]);
+
+        aggregate_1 = _bn128_add([resultMult_1[0], resultMult_1[1], aggregate_1[0], aggregate_1[1]]);
+        aggregate_2 = _bn128_add([resultMult_2[0], resultMult_2[1], aggregate_2[0], aggregate_2[1]]);
         }
 
+      uint256[4] memory aggregate = [aggregate_1[0], aggregate_1[1], aggregate_2[0], aggregate_2[1]];
       emit Aggregate(aggregate);
       return aggregate;
     }
