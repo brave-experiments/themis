90f008ad0d0f0b1603d5244069314afa2a4744d5
gpestana <g6pestana@gmail.com>
2020-07-29 17:35:57 +0100

    ec2 sidechain measurements

---
+++ themis-measurements/tooling/scp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for server in $(cat hosts)
+do
+	scp -i ~/Desktop/gpestana-themis.pem ~/brave/decentralized-bat-net-experiments/themis-client-rs/src/lib.rs "$server":/home/ec2-user/themis-client-rs/src
+	scp -i ~/Desktop/gpestana-themis.pem ~/brave/decentralized-bat-net-experiments/themis-client-rs/examples/full-e2e-benchmark.rs "$server":/home/ec2-user/themis-client-rs/examples
+done
